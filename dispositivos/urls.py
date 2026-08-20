f83b4fa25cc6e8e58f01680d79871e0dfaf6889b
Laboratorio Computación N° 9 <claseslab9@inacap.cl>
2026-08-20 10:17:27 -0400

    Clase3

---
+++ dispositivos/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from . import views
+app_name = "dispositivos"
+
+urlpatterns = [
+    path("", views.inicio, name="inicio"),
+    path(
+    "zonas/<int:zona_id>/dispositivos/",
+    views.dispositivos_zona,
+    name="por_zona",
+),
+    path(
+    "games/<int:game_id>/dispositivos/",
+    views.dispositivos_game,
+    name="por_game",
+)
+]
